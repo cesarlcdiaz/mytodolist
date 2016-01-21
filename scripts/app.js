09264385ae382787c9dbcf43db3d5fb2690cb21f
Cesar Alvarez <Cesar Alvarez>
2016-01-21 12:19:38 -0800

    #1 User can edit task.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+
+.controller('mainCtrl', function($scope) {
+
+  $scope.learningNgChange = function() {
+
+    console.log("hello world message");
+    
+  };
+
+  $scope.todos = [
+
+  {"name": "clean the house"},
+  {"name": "pay the bills"},
+  {"name": "feed the dog"},
+  {"name": "water the plants"},
+  {"name": "change the lights"},
+  {"name": "work on projects"},
+  {"name": "get Money!"}
+
+  ]
+});
